d5803cad80a958f5b6874023a9a80b74d3a2113f
Digital Charlie <furman.jr@gmail.com>
2022-04-27 10:59:51 -0700

    sort order testing on character home page

--- src/components/CharShow/CharShow.jsx
+++ src/components/CharShow/CharShow.jsx
@@ -7,9 +7,36 @@ export default function UserHome({user}){
 	const [char, setChar] = useState({})
 	const [advs, setAdvs] = useState([])
 	const [magicItems, setmagicItems] = useState([])
+	const [sortType, setSortType ] = useState(null)
+	const [sortOrder, setSortOrder] = useState(true)
 
 	const navigate = useNavigate()
 
+	const sortAdventures = (x, y) => {
+		let a = ''
+		let b = ''
+		if (sortType === 'name') {
+			 a = x.adventureName.toUpperCase();
+			 b = y.adventureName.toUpperCase();
+		} else {
+			 a = new Date(x.hireDate);
+       		 b = new Date(y.hireDate);
+		}
+		if (sortOrder === true) {
+			return a === b ? 0 : a > b ? 1 : -1;
+		} else {
+			return a === b ? 0 : a > b ? -1 : 1;
+		}
+	}
+
+	const handleAdventureSort = (type) => {
+		if (sortType === type) {
+			setSortOrder(!sortOrder)
+		} else {
+			setSortType(type)
+			setSortOrder(true)
+		}
+	}
 
 	useEffect(() => {
 		(async () => {
@@ -18,14 +45,14 @@ export default function UserHome({user}){
 				setChar(charData)
 				const advData = await charAPI.getAllAdv(user.username, charId)
 				setAdvs(advData)
+				setAdvs(advData.sort(sortAdventures))
 				const magicData = await charAPI.getAllMagic(user.username, charId)
 				setmagicItems(magicData)
-				console.log(magicData)
 			} catch(e) {
 				console.log(e)
 			}
 		})()
-	}, [])
+	}, [sortOrder, sortType])
 
     return (
         <main>
@@ -33,6 +60,8 @@ export default function UserHome({user}){
             <p>Character name is: {char.name}</p>
 			<hr />
 			<p>Adventures</p>
+			<button onClick={()=>handleAdventureSort('name')}>Sort by adventure name</button>
+			<button onClick={()=>handleAdventureSort('date')}>Sort by adventure date</button>
 			{
 				advs.map((adventure) => (
 					<p key={adventure._id}><Link to={`/user/${user.username}/character/${charId}/adventure/${adventure._id}/`}>{adventure.adventureName}</Link></p>
